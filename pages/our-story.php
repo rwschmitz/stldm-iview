2efee235a480dfc4af9a3686a10d2f1613047e1d
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 10:13:08 -0500

    adding content

--- pages/our-story.php
+++ pages/our-story.php
@@ -34,28 +34,14 @@
                 <h1><?php get_post_meta(get_the_ID(), 'our-story'); ?></h1>
                 <h1><?php the_post(); ?></h1>
                 <h1>ensuring this container even works</h1>
+                <h1><?php the_content(); ?></h1>
             </div> <!--- End col -->
         </div> <!-- End row -->
     </div> <!-- End container -->
 
 <!-- does this work-->
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html_e( 'Pages:', 'clean-lite' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-	<?php edit_post_link( esc_html_e( 'Edit', 'clean-lite' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-</article><!-- #post-## -->
 
 <!-- does this work end -->
 
