f1e448e6f5cf3022369365661e4dd811db1a1f95
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 13:21:20 -0500

    adding home.php

--- home.php
+++ home.php
@@ -3,9 +3,16 @@
 
 <body>
 
-
-
-
+    <div class="container">
+        <div class="jumbotron-fluid pt-5 pb-3 text-center">
+            <h1 class="display-3">
+                <?php echo get_post_meta(get_the_ID(), 'home-jumbotron', true); ?>
+            </h1> <!-- End display -->
+            <p class="lead">
+                <?php echo get_post_meta(get_the_ID(), 'home-sub-jumbotron', true); ?>
+            </p>   <!-- End lead -->
+        </div> <!-- End jumbotron -->
+    </div> <!-- End container -->
     
     <div class="container py-3">
         <div class="row">
--- index.php
+++ index.php
@@ -5,24 +5,7 @@
 
 
 
-    <div class="container">
-        <div class="jumbotron-fluid pt-5 pb-3 text-center">
-            <h1 class="display-3">
-                Rudolph Schmitz - Web Developer
-            </h1> <!-- End display -->
-            <p class="lead">
-                As of 10/23/2017, I've been alive for 10,685.  This is my best streak so far and I look forward to topping it tomorrow.
-            </p>   <!-- End lead -->
-        </div> <!-- End jumbotron -->
-    </div> <!-- End container -->
-    
-    <div class="container py-3">
-        <div class="row">
-            <div class="col-xl-12">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac aliquam arcu. Morbi tellus lectus, lobortis eget tellus vitae, commodo ultricies magna. Proin blandit scelerisque magna vel iaculis. Mauris feugiat enim id dui facilisis molestie. Nunc rhoncus viverra lectus, ut mattis nisl tristique in. Ut in tellus ultricies, porta purus vitae, imperdiet elit. Maecenas pellentesque nulla nunc, quis molestie sapien blandit nec. Duis tristique fermentum ex, sit amet gravida ligula lobortis sit amet. Nam non interdum erat.
-            </div> <!-- End col -->
-        </div> <!-- End row -->
-    </div> <!-- End container -->
+
 
     <div class="container py-3">
         <div class="row">
@@ -36,23 +19,6 @@
             </div> <!-- End col -->
         </div> <!-- End row -->
     </div> <!-- End container -->
-
-    <div class="container py-3">
-        <div class="row">
-            <div class="col-xl-12 pb-3">
-                Sed nunc leo, rhoncus nec lobortis eget, consequat at risus. Quisque scelerisque velit ante, et euismod tellus lacinia quis. Integer malesuada eros et cursus placerat. Fusce ultrices dolor sit amet porta eleifend. Donec euismod vitae erat in dictum. Donec varius tortor consectetur, iaculis odio eu, ultricies diam. Aliquam gravida sed tellus eget tincidunt. Phasellus fringilla odio commodo, iaculis dui ut, ultrices elit. Maecenas blandit varius quam in cursus. Aliquam pharetra lacus ligula, tempus malesuada erat rhoncus in. Cras nec auctor tellus. Fusce sit amet arcu nec leo aliquet laoreet. Fusce non erat eget purus tempus aliquet. Etiam eu aliquet nisi, eget fermentum leo. Vivamus quis neque eget augue blandit sagittis. Sed vulputate, ipsum at cursus vehicula, quam mi fermentum enim, in convallis nunc tortor a enim.
-            </div> <!-- End col -->
-        </div> <!-- End row -->
-
-        <div class="row">
-            <div class="col-xl-12">
-                Sed nunc leo, rhoncus nec lobortis eget, consequat at risus. Quisque scelerisque velit ante, et euismod tellus lacinia quis. Integer malesuada eros et cursus placerat. Fusce ultrices dolor sit amet porta eleifend. Donec euismod vitae erat in dictum. Donec varius tortor consectetur, iaculis odio eu, ultricies diam. Aliquam gravida sed tellus eget tincidunt. Phasellus fringilla odio commodo, iaculis dui ut, ultrices elit. Maecenas blandit varius quam in cursus. Aliquam pharetra lacus ligula, tempus malesuada erat rhoncus in. Cras nec auctor tellus. Fusce sit amet arcu nec leo aliquet laoreet. Fusce non erat eget purus tempus aliquet. Etiam eu aliquet nisi, eget fermentum leo. Vivamus quis neque eget augue blandit sagittis. Sed vulputate, ipsum at cursus vehicula, quam mi fermentum enim, in convallis nunc tortor a enim.
-            </div> <!-- End col -->
-        </div> <!-- End row -->
-
-    </div> <!-- End container -->
-
-
 
 
 
