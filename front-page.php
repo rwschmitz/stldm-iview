0ececefabed3c94edf72595807edf46052d62940
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 13:45:35 -0500

    troubleshooting front-page.php

--- front-page.php
+++ front-page.php
@@ -3,9 +3,11 @@
 <?php // Show the selected frontpage content.
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post();
-				get_template_part( 'front-page' );
+				get_template_part( 'template-parts/page/content', 'front-page' );
 			endwhile;
-        endif; ?>
+		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
+			get_template_part( 'template-parts/post/content', 'none' );
+		endif; ?>
 
     <div class="container">
         <div class="jumbotron-fluid pt-5 pb-3 text-center">
