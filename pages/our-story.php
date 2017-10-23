140d684ece9f4c67512360b4e8985f19e1637901
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 10:39:21 -0500

    enabling user input for titles and content

--- pages/our-story.php
+++ pages/our-story.php
@@ -3,15 +3,15 @@
 
 
 <!-- Landing page start -->
-<div class="no-gutters">
+
 
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
                 <div class="test">
                     <div class="header">
-                        <?php echo get_post_meta(get_the_ID(), 'main-header', true); ?> // php comments tho
-                    </h1> <
+                        <?php echo get_post_meta(get_the_ID(), 'main-header', true); // Allows the user to change the title of each page from the Wordpress dashboard ?> 
+                    </div>
                 </div> <!-- End test -->
             </div> <!--- End col -->
         </div> <!-- End row -->
@@ -21,13 +21,15 @@
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
-                <h1><?php the_content(); ?></h1>
+                <div class="content">>
+                    <?php the_content(); // Allows the user to change the page content from the Wordpress dashboard?>
+                </div>
             </div> <!--- End col -->
         </div> <!-- End row -->
     </div> <!-- End container -->
 
 
-</div> <!-- End no-gutters -->
+
 
 
 
