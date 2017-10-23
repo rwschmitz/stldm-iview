e8c0e7c6cbb07ddf1c9ed8991c44de0f0cfc7b70
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 12:24:54 -0500

    adding jumbotron and info

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
 
 
 
-    <!-- Landing page start -->
-    <div class="no-gutters">
 
-
-    </div> <!-- End no-gutters -->
+    <div class="jumbotron-fluid">
+         <h1 class="display-1">
+            Rudolph Schmitz - Web Developer
+        </h1>
+        <p class="lead">
+            As of 10/23/2017, I've been alive for 10,685.  This is my best streak so far and I look forward to topping it tomorrow.
+        </p>   
+    </div> 
 
 
 
