f904687bec22e8efa86f6eec4b0ab7c43707ce3b
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 08:24:33 -0500

    test update index.php

--- index.php
+++ index.php
@@ -8,19 +8,23 @@
     <!-- Landing page start -->
     <div class="no-gutters landing">
         blah blah blah
-    </div> <!-- End no-gutters -->
 
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-12">
-                hello<br>
-                hello<br>
-                hello<br>
-                hello<br>
-                hello<br>
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    hello<br>
+                    hello<br>
+                    hello<br>
+                    hello<br>
+                    hello<br>
+                </div>
             </div>
         </div>
-    </div>
+
+
+    </div> <!-- End no-gutters -->
+
+
 
 
 
