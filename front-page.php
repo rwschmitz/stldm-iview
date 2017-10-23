7acf48725cabb39cbbbb8741b4f321c159bb8774
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 13:46:42 -0500

    troubleshooting front-page.php

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 <?php // Show the selected frontpage content.
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
+				get_template_part( 'front-page' );
 			endwhile;
 		endif; ?>
 
