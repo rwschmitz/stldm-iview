4a4728d096a77939c9d172ff23884a729340330c
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 09:39:26 -0500

    adding custom fields

--- pages/our-story.php
+++ pages/our-story.php
@@ -8,8 +8,20 @@
 <div class="container">
     <div class="row">
         <div class="col-xl-12">
+            <div class="test">
+                <h1><?php echo get_post_meta(get_the_ID(), 'test-header', true); ?></h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-xl-12">
             <h1> this is a story all about how my life got flipped, turned upside down.  and i'd like to take a minute, just right there
-                and i'll tell you all about how i become the Fresh Prince of Bel-Air.
+                and i'll tell you all about how i become the Prince of Bel-Air.
              </h1>
         </div>
     </div>
