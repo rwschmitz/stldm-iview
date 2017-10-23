18e97e8e7a0c57add1a18e829c2fac57cd462588
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 08:20:08 -0500

    removing footer

--- footer.php
+++ footer.php
@@ -5,14 +5,14 @@
 
 <?php get_sidebar(); ?>
 
-<div class="container">
+<!--<div class="container">
     <div class="row">
         <div class="col-xl-12 d-flex justify-content-center">
             <footer class="copyright">Demo theme, ©2017 | Developed by:  Rudolph Schmitz</footer>
             <?php wp_footer(); ?>
         </div>
     </div>
-</div>
+</div>-->
 
 
     </body>
