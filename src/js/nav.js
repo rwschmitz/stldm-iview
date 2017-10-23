e8452eb1fe8bd1a499e625975b7b5564b6c10be1
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 17:14:27 -0500

    nav.js with media queries

--- src/js/nav.js
+++ src/js/nav.js
@@ -49,10 +49,6 @@ function mobileDrop(e) { // Determines which drop down menu to show
     } 
  }
 
- if (window.matchMedia('(max-width: 991px)').matches && flag[0] === true) {
-     listeners[0].addEventListener("click", mobileNoDrop, false);
- }
-
 function mobileNoDrop() {
     if (flag[0]) {
         $(function() {
