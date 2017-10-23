59f771e18249939267262d28b8253072c270fc07
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 13:15:26 -0500

    adding home.php

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+
+<body>
+
+
+
+
+    
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-xl-12">
+                <?php the_content(); ?>
+            </div> <!-- End col -->
+        </div> <!-- End row -->
+    </div> <!-- End container -->
+
+   
+
+
+
+
+
+
+<?php get_footer(); ?>
