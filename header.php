e6a229bcea6bc1aa6a128155e74ae7eefcfe0fbd
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 15:54:15 -0500

    updating nav.js

--- header.php
+++ header.php
@@ -31,15 +31,15 @@
             <a href="https://newtpress.com/stldm-iview/menu-item-1" class="nav-link nav-item"  data-target=".navbar-collapse.show"><button id="goHome" class="nav-btn underline">menu item 1</button></a>
             <a href="https://newtpress.com/stldm-iview/menu-item-2" class="nav-link nav-item"  data-target=".navbar-collapse.show"><button id="goMenu" class="nav-btn underline">menu item 2</button></a>
 
-            <button class="nav-btn underline">menu item 3
-            <a id="mainDropDown1" class="nav-link nav-item" data-target=".navbar-collapse.show"></a></button>
-             
-                    <div id="subdrop1" class="dropdown-menu">
+
+            <a href="https://newtpress.com/stldm-iview/menu-item-3" id="mainDropDown1" class="nav-link nav-item" data-target=".navbar-collapse.show">
+                <button id="subdrop1" class="nav-btn underline dropdown-menu">menu item 3</button>
+                   <!-- <div id="subdrop1" class="dropdown-menu">-->
                         <li><button class="nav-drop-btn underline"><a href="google.com">Sub-menu item 1</a></button></li>
                         <li><button class="nav-drop-btn underline"><a href="nhl.com">Sub-menu item 2</a></button></li>
                         <li><button class="nav-drop-btn underline"><a href="apple.com">Sub-menu item 3</a></button></li>
-                    </div>
-            <!--</a>-->
+                    <!--</div>-->
+            </a>
             
 
 
