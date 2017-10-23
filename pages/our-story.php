125b0e9e3ccd0e694156a152e7bef90aed026e86
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 10:11:56 -0500

    adding content

--- pages/our-story.php
+++ pages/our-story.php
@@ -38,6 +38,27 @@
         </div> <!-- End row -->
     </div> <!-- End container -->
 
+<!-- does this work-->
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html_e( 'Pages:', 'clean-lite' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	<?php edit_post_link( esc_html_e( 'Edit', 'clean-lite' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+</article><!-- #post-## -->
+
+<!-- does this work end -->
+
 
 </div> <!-- End no-gutters -->
 
