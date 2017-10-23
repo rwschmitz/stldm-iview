1c38a1aafbbfc3b41fa4cc311d431b1b1917188b
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 13:43:50 -0500

    troubleshooting front-page.php

--- front-page.php
+++ front-page.php
@@ -21,9 +21,6 @@
     <div class="container py-3">
         <div class="row">
             <div class="col-xl-12">
-                ?????????????
-                <?php the_excerpt(); ?>
-                <?php the_content(); ?>
                 <?php the_post(); ?>
             </div> <!-- End col -->
         </div> <!-- End row -->
