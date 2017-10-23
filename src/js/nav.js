572e571942800c43cdb94b826cc23e181634a4e5
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2017-10-23 17:29:44 -0500

    nav.js with media queries

--- src/js/nav.js
+++ src/js/nav.js
@@ -37,7 +37,7 @@ function noDrop() { // Determins which drop down menu to hide
 
 
 var listeners = [document.getElementById("mobileDropDown")]; // Puts all the elements in an array
-listeners[0].addEventListener("click", mobileDrop, false);
+
 
 function mobileDrop(e) { // Determines which drop down menu to show
     var target = $(e.target);
@@ -56,6 +56,6 @@ function mobileNoDrop() {
            $('#subdrop1').fadeOut(250).removeClass("show");
         });
         flag[0] = false;
-
+        listeners[0].addEventListener("click", mobileDrop, false);
      }
 }
